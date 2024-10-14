c23948f37adac546325150ee96c7a08a2179d776
Nandugokul <iamnandugokul@gmail.com>
2024-10-14 21:52:18 +0530

    responsiveness completed

--- src/components/ui/FilterSection.jsx
+++ src/components/ui/FilterSection.jsx
@@ -69,86 +69,104 @@ function FilterSection() {
   }, []);
 
   return (
-    <section className="flex items-center justify-between py-5">
-      <div className="flex space-x-4 items-center">
-        <select
-          onChange={handleFilterChange}
-          className="w-1/3 md:max-w-36 "
-          name="category"
-          value={filterState.category}
-        >
-          <option value="" disabled>
-            Category
-          </option>
-          {categoryList.map((category, index) => (
-            <option key={category + index} value={category}>
-              {category}
+    <section>
+      <div className="flex items-center justify-between py-5">
+        <div className="flex space-x-4 items-center">
+          <select
+            onChange={handleFilterChange}
+            className="w-1/3 md:max-w-36 "
+            name="category"
+            value={filterState.category}
+          >
+            <option value="" disabled>
+              Category
             </option>
-          ))}
-        </select>
+            {categoryList.map((category, index) => (
+              <option key={category + index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
 
-        <select
-          value={filterState.rating}
-          onChange={handleFilterChange}
-          className="w-1/3 md:max-w-36"
-          name="rating"
-        >
-          <option value="" disabled>
-            Rating
-          </option>
-          <option value="3">Above 3</option>
-          <option value="4">Above 4</option>
-        </select>
-        <button
-          disabled={!filterState.category && !filterState.rating}
-          onClick={handleReset}
-          className={`${
-            !filterState.category && !filterState.rating && "text-[#9e9f9f]"
-          } text-my14 flex items-center space-x-1 opacity-70`}
-        >
-          <MdOutlineReplay />
-          <span className="hidden sm:block">Reset</span>
-        </button>
-      </div>
-      <div className="flex items-center space-x-6">
-        {tempProductList.length && layout === "list" ? (
-          <div className="flex items-center space-x-2 ">
-            <span className="text-lg font-semibold">
-              {tempProductList.length}
-            </span>
-            <span>products selected</span>
-            <span>
-              <button
-                onClick={handleAddToCart}
-                className="ml-1 font-semibold underline underline-offset-2"
-              >
-                Add To Cart
-              </button>
-            </span>
-          </div>
-        ) : null}
-
-        <div className="flex ms-4">
-          <button
-            className={` rounded-md p-2 text-[#9e9f9f] ${
-              layout === "grid" ? "bg-selectBG text-black" : null
-            }`}
-            onClick={() => handleLayoutChange("grid")}
+          <select
+            value={filterState.rating}
+            onChange={handleFilterChange}
+            className="w-1/3 md:max-w-36"
+            name="rating"
           >
-            <TbLayoutGrid className="text-xl" />
-          </button>
+            <option value="" disabled>
+              Rating
+            </option>
+            <option value="3">Above 3</option>
+            <option value="4">Above 4</option>
+          </select>
           <button
-            className={`rounded-md p-2 text-[#9e9f9f] ${
-              layout === "list"
-                ? "bg-selectBG text-text-[#9e9f9f] text-black"
-                : null
-            }`}
-            onClick={() => handleLayoutChange("list")}
+            disabled={!filterState.category && !filterState.rating}
+            onClick={handleReset}
+            className={`${
+              !filterState.category && !filterState.rating && "text-[#9e9f9f]"
+            } text-my14 flex items-center space-x-1 opacity-70`}
           >
-            <TbLayoutList className="text-xl" />
+            <MdOutlineReplay />
+            <span className="hidden sm:block">Reset</span>
           </button>
         </div>
+        <div className="flex items-center space-x-6">
+          {tempProductList.length && layout === "list" ? (
+            <div className="items-center space-x-2 hidden md:flex">
+              <span className="text-lg font-semibold">
+                {tempProductList.length}
+              </span>
+              <span>products selected</span>
+              <span>
+                <button
+                  onClick={handleAddToCart}
+                  className="ml-1 font-semibold underline underline-offset-2"
+                >
+                  Add To Cart
+                </button>
+              </span>
+            </div>
+          ) : null}
+          <div className="flex ms-4">
+            <button
+              className={` rounded-md p-2 text-[#9e9f9f] ${
+                layout === "grid" ? "bg-selectBG text-black" : null
+              }`}
+              onClick={() => handleLayoutChange("grid")}
+            >
+              <TbLayoutGrid className="text-xl" />
+            </button>
+            <button
+              className={`rounded-md p-2 text-[#9e9f9f] ${
+                layout === "list"
+                  ? "bg-selectBG text-text-[#9e9f9f] text-black"
+                  : null
+              }`}
+              onClick={() => handleLayoutChange("list")}
+            >
+              <TbLayoutList className="text-xl" />
+            </button>
+          </div>
+        </div>
       </div>
+
+      {tempProductList.length && layout === "list" ? (
+        <div className="flex items-center md:hidden space-x-2 pb-4">
+          <span className="text-lg font-semibold">
+            {tempProductList.length}
+          </span>
+          <span>products selected</span>
+          <span>
+            <button
+              onClick={handleAddToCart}
+              className="ml-1 font-semibold underline underline-offset-2"
+            >
+              Add To Cart
+            </button>
+          </span>
+        </div>
+      ) : null}
     </section>
   );
 }
