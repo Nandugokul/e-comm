fc531697638a36e7678118895b26e2f1cba6d8c5
Nandugokul <iamnandugokul@gmail.com>
2024-10-14 21:05:34 +0530

    responsiveness

--- src/components/ui/ProductListingRow.jsx
+++ src/components/ui/ProductListingRow.jsx
@@ -66,35 +66,74 @@ function ProductListingRow({ product }) {
   };
 
   return (
-    <section className="grid grid-cols-9 md:grid-cols-8  items-center p-4 border-b rounded-md gap-2">
-      <div className="col-span-full md:col-span-4 flex items-center space-x-5">
+    <section className="grid grid-cols-3 gap-y-4 md:gap-4 md:gap-y-4 md:grid-cols-8   items-center p-4 border-b rounded-md gap-2">
+      <div className="col-span-2 md:col-span-4 flex items-center md:space-x-4 relative">
         <img
-          className="group-hover:scale-105 transition-all duration-150 ease-in-out w-[100px] h-[100px] object-cover"
+          className="group-hover:scale-105 transition-all  duration-150 ease-in-out w-[100px] h-[100px] object-cover"
           src={product.thumbnail}
           alt={product.title}
           loading="lazy"
         />
         <div className="md:max-w-48">
-          <h6 className="font-semibold text-lg">{product.title}</h6>
+          <h6 className="font-semibold md:text-lg ms-3 md:ms-4">
+            {product.title}
+          </h6>
         </div>
+        {soldOut && (
+          <small className="absolute top-0 left-0 rounded-md px-4 py-1 bg-selectBG text-[0.7rem]">
+            Sold Out
+          </small>
+        )}
       </div>
-      <div className="col-span-2 md:col-span-1">
+      <div className="md:hidden">
+        {soldOut ? (
+          <div className="col-span-2 md:col-span-1 text-end">----</div>
+        ) : (
+          <div className="flex items-center justify-end col-span-2 md:col-span-1">
+            <label
+              className={`${
+                quantity ? "bg-primaryColor" : ""
+              } ms-4 grid  top-4 h-5 w-5 px-[2px] border-2 rounded-[4px] border-primaryColor cursor-pointer`}
+              htmlFor={`${"checkbox " + product.id}`}
+            >
+              {quantity ? <FaCheck className="text-white w-3" /> : null}
+            </label>
+
+            <input
+              onChange={handleSelectionChange}
+              id={`${"checkbox " + product.id}`}
+              type="checkbox"
+              className="hidden"
+            />
+          </div>
+        )}
+      </div>
+
+      <div className="md:col-span-1">
         <div className="flex space-x-1 items-center bg-selectBG px-1 mb-1 rounded-[4px] w-fit">
           <img src={starIcon} className="w-3" alt="stars" />
           <small>{product.rating.toFixed(1)}</small>
         </div>
         <small className="text-gray-500">{reviewCount} reviews</small>
       </div>
-      <h2 className={`font-semibold col-span-2 md:col-span-1`}>
-        ${product.price}
-      </h2>
+      <h2 className={`font-semibold `}>${product.price}</h2>
       {soldOut ? (
-        <div className="text-center col-span-3 md:col-span-1">
-          <div>----</div>
+        <div className=" text-end">
+          <div className="hidden md:block col-span-2 md:col-span-1 text-center">
+            ----
+          </div>
+          <button
+            onClick={() => {
+              toast.success("You will be notified on stock availability");
+            }}
+            className="md:hidden border-primaryColor border font-semibold text-primaryColor col-span-2 md:col-span-1 py-2 px-4 rounded-md"
+          >
+            Notify
+          </button>
         </div>
       ) : (
         <div
-          className={` grid  col-span-3 md:col-span-1 grid-cols-3 items-center w-fullljustify-between border-borderColor border rounded-md`}
+          className={` grid  grid-cols-3 items-center w-fullljustify-between border-borderColor border rounded-md`}
         >
           <button
             disabled={quantity === 0}
@@ -112,36 +151,38 @@ function ProductListingRow({ product }) {
           </button>
         </div>
       )}
-      {soldOut ? (
-        <div className="col-span-2 md:col-span-1 text-end">
-          <button
-            onClick={() => {
-              toast.success("You will be notified on stock availability");
-            }}
-            className="border-primaryColor border font-semibold text-primaryColor col-span-2 md:col-span-1 py-2 px-4 rounded-md"
-          >
-            Notify
-          </button>
-        </div>
-      ) : (
-        <div className="flex items-center justify-end col-span-2 md:col-span-1">
-          <label
-            className={`${
-              quantity ? "bg-primaryColor" : ""
-            } ms-4 grid  top-4 h-5 w-5 px-[2px] border-2 rounded-[4px] border-primaryColor cursor-pointer`}
-            htmlFor={`${"checkbox " + product.id}`}
-          >
-            {quantity ? <FaCheck className="text-white w-3" /> : null}
-          </label>
+      <div className="hidden md:block">
+        {soldOut ? (
+          <div className="col-span-2 md:col-span-1 text-end">
+            <button
+              onClick={() => {
+                toast.success("You will be notified on stock availability");
+              }}
+              className="border-primaryColor border font-semibold text-primaryColor col-span-2 md:col-span-1 py-2 px-4 rounded-md"
+            >
+              Notify
+            </button>
+          </div>
+        ) : (
+          <div className="flex items-center justify-end col-span-2 md:col-span-1">
+            <label
+              className={`${
+                quantity ? "bg-primaryColor" : ""
+              } ms-4 grid  top-4 h-5 w-5 px-[2px] border-2 rounded-[4px] border-primaryColor cursor-pointer`}
+              htmlFor={`${"checkbox " + product.id}`}
+            >
+              {quantity ? <FaCheck className="text-white w-3" /> : null}
+            </label>
 
-          <input
-            onChange={handleSelectionChange}
-            id={`${"checkbox " + product.id}`}
-            type="checkbox"
-            className="hidden"
-          />
-        </div>
-      )}
+            <input
+              onChange={handleSelectionChange}
+              id={`${"checkbox " + product.id}`}
+              type="checkbox"
+              className="hidden"
+            />
+          </div>
+        )}
+      </div>
     </section>
   );
 }
